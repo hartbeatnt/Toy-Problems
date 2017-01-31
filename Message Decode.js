9a2f411867c33dbb796006a3536673383bdf15ee
Nathaniel Hart <Nathaniel Hart>
2017-01-30 19:31:11 -0800

    more test cases

--- Message Decode.js
+++ Message Decode.js
@@ -15,21 +15,72 @@ console.log (device.encode ('What the hell')) ;
 Our cryptoanalysts kept poking at it and found some interesting patterns.
 
 console.log (device.encode ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')) ;
+==> bdhpF,82QsLirJejtNmzZKgnB3SwTyXG ?.6YIcflxVC5WE94UA1OoD70MkvRuPqHabdhpF,82QsLir
 console.log (device.encode ('bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb')) ;  
+==> dhpF,82QsLirJejtNmzZKgnB3SwTyXG ?.6YIcflxVC5WE94UA1OoD70MkvRuPqHabdhp
+console.log (device.encode ('ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc')) ;
+==> flxVC5WE94UA1OoD70MkvRuPqHabdhpF,82QsLirJejtNmzZKgnB3SwTyXG
+console.log (device.encode ('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')) ;
+==> 1OoD70MkvRuPqHabdhpF,82QsLirJejtNmzZKgnB3SwTyXG ?.6YIcf
+console.log (device.encode ('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB')) ;
+==> 3SwTyXG ?.6YIcflxVC5WE94UA1OoD70MkvRuPqHabdhpF,82QsLirJejtNmzZKgnB3SwTyXG ?.6Y
 console.log (device.encode ('!@#$%^&*()_+-')) ;
-console.log ('abcdefghijklmnopqrstuvwxyz') ;
+==> !@#$%^&*()_+-
+console.log (device.encode('abcdefghijklmnopqrstuvwxyz')) ;
+==> bhx,zWyLZ3pOGIhzeXTYtjAaDW
 console.log ('abcdefghijklmnopqrstuvwxyz'.split ('').map (function (a) {
   return device.encode (a) ;
 }).join ('')) ;
+==> bdfhjlnprtvxzBDFHJLNPRTVXZ
 console.log ('abcdefghijklmnopqrstuvwxyz'.split ('').map (function (a) {
   return device.encode ('_'+a)[1] ;
 }).join ('')) ;
+==> dhlptxBFJNRVZ37,aeimquyCGK
 console.log ('abcdefghijklmnopqrstuvwxyz'.split ('').map (function (a) {
   return device.encode ('__'+a)[2] ;
 }).join ('')) ;
+==> hpxFNV3,emuCKS08bjrzHPX5 g
+console.log ('aaaaaaaaaaaaaaaaaaaaaaaaaa'.split ('').map (function (a) {
+  return device.encode (a) ;
+}).join ('')) ;
+==> bbbbbbbbbbbbbbbbbbbbbbbbbb
+console.log ('aaaaaaaaaaaaaaaaaaaaaaaaaa'.split ('').map (function (a) {
+  return device.encode ("_"+a)[1] ;
+}).join ('')) ;
+==> ddddddddddddddddddddddddddddd
+console.log ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa'.split ('').map (function (a) {
+  return device.encode ("__"+a)[2] ;
+}).join ('')) ;
+==> hhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+console.log ('aaaaaaaaaaaaaaaaaaaaaaaaaaaaa'.split ('').map (function (a) {
+  return device.encode ("___"+a)[3] ;
+}).join ('')) ;
+==> ppppppppppppppppppppppppppppp
+console.log ('zzzzzzzz'.split ('').map (function (a) {
+  return device.encode (a) ;
+}).join ('')) ;
+==> ZZZZZZZZ
+console.log ('ZZZZZZZZ'.split ('').map (function (a) {
+  return device.encode ("___"+a)[3] ;
+}).join ('')) ;
+==> BBBBBBBB
+
+
+
 We think if you keep on this trail you should be able to crack the code! You are expected to fill in the
 
 device.decode
 function. Good luck ! General Patron is counting on you!
  */
 
+device.decode = function (w) {
+  let result = '';
+  let characters = 
+    ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+     'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+     '0','1','2','3','4','5','6','7','8','9','.',',','?',' ']
+  w.split('').forEach(chr=>{
+    let idx = characters.indexOf()
+  })
+  return result ; 
+}
