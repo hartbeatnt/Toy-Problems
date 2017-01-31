a9fc77db81a6cbd896426a0b51c84424cee93cf6
Nathaniel Hart <Nathaniel Hart>
2017-01-31 11:48:11 -0800

    working on algorithm

--- Message Decode.js
+++ Message Decode.js
@@ -78,9 +78,14 @@ device.decode = function (w) {
   let characters = 
     ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
      'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-     '0','1','2','3','4','5','6','7','8','9','.',',','?',' ']
-  w.split('').forEach(chr=>{
-    let idx = characters.indexOf()
+     '0','1','2','3','4','5','6','7','8','9','.',',','?',' ','$']
+  w.split('').forEach((chr,i)=>{
+    let idx = characters.indexOf(chr)
+    let pos = Math.pow(2, i+1)*chr;
+    while (pos > characters.length) {
+      pos -= characters.length;
+    }
+    result.push(characters[pos])
   })
   return result ; 
 }
