c87d5a2c53ee124a48b86862af636408b8d1cb2d
Nathaniel Hart <Nathaniel Hart>
2017-02-01 10:48:37 -0800

    starting Break The Pieces

---
+++ Hard Time Bomb.js
@@ -0,0 +1,12 @@
+/**
+ from https://www.codewars.com/kata/hard-time-bomb/train/javascript
+ 
+ A bomb has been set to go off! You have to find the wire and cut 
+ it in order to stop the timer. There is a global var that holds 
+ the numeric ID to which wire to cut. Find that and then you can 
+ Bomb.CutTheWire(wireKey);
+ */
+
+// console.log(global)
+// find property that starts with "boom"
+// pass the value at that key into cutTheWire function
