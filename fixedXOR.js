6142a7d06b4b21c6b4af56067f3bc8324f104e68
Nathaniel Hart <Nathaniel Hart>
2017-02-04 10:56:54 -0800

    completed fixedXOR

--- fixedXOR.js
+++ fixedXOR.js
@@ -16,13 +16,34 @@ let buffer1 = '1c0111001f010100061a024b53535009181c';
 let buffer2 = '686974207468652062756c6c277320657965';
 let xor = '746865206b696420646f6e277420706c6179';
 
+const hexToBinary = hex => {
+  let bytes = [];
+  for(let i=0; i < hex.length-1; i+=2) {
+    // two hexadecimal values = one byte
+    let hexByte = hex.substring(i, i+2);
+    // convert hexadecimal to decimal
+    let binByte = parseInt(hexByte, 16);
+    // convert decimal to binary
+    binByte = binByte.toString(2);
+    // replace leading zeros removed by JS
+    while (binByte.length < 8) binByte = '0'+binByte;
+    bytes.push(binByte);
+  }
+  return bytes.join('')
+}
 
-const fixedXOR = (buffer1, buffer2) => {
-  // decode buffer 1 hex to binary
-  // decode buffer 2 hex to binary
-  // xor buff1bin against buff2bin
-  // encode the result in hexadecimal
+const fixedXOR = (bin1, bin2) => {
+  return bin1.split('').map((bit,i)=> bin1[i]===bin2[i] ? '0':'1').join('')
+}
 
+const binaryToHex = bin => {
+  let nibbles = []
+  for (var i=0; i < bin.length-3; i+=4) {
+    nibbles.push(bin.substring(i,i+4))
+  }
+  nibbles = nibbles.map(v=>parseInt(v,2))
+  nibbles = nibbles.map(v=>v.toString(16))
+  return nibbles.join('')
 }
 
 const assert = (expected, actual, description) => {
@@ -33,5 +54,5 @@ const assert = (expected, actual, description) => {
      got: ${actual}`)
 }
 
-assert(xor, fixedXOR(buffer1,buffer2), 
+assert(xor, binaryToHex(fixedXOR(hexToBinary(buffer1),hexToBinary(buffer2))), 
   "should return the XOR combination of 2 fixed-length buffers")
