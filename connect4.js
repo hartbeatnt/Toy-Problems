d9b4b940ef6063c31617caf3488920349aa18538
Nathaniel T. Hart <hartbeatnt@gmail.com>
2017-02-28 10:24:29 -0800

    completed console connect 4 implementation

--- connect4.js
+++ connect4.js
@@ -1,74 +1,113 @@
-/*
-implement connect 4 in the console
-*/
-
 const Board = function(){
   this.cols = [];
   for (let i = 0; i < 7; i++) {
     this.cols[i] = new Array(6).fill(null)
   }
-  this.winner = null;
   this.turn = Math.random() > 0.5 ?
     'X' : 'O';
 }
 
 Board.prototype.placePiece = function(col) {
+  this.winner = null;
   if (!this.cols[col]) return 'invalid move. Try again'
   let placed = false;
   this.cols[col].forEach((cell, i)=>{
     if (!cell && !placed) {
       this.cols[col][i] = this.turn;
       placed = true;
-      this.winCheck(i, this.cols[col])
+      this.winCheck(i, col)
     }
   })
   if (!placed) return 'invalid move. Try again'
   this.printBoard()
-  if (this.winner) return `${this.winner} wins!`
-  this.turn = this.turn === 'X' ?
-    'O' : 'X'
+  if (this.winner) {
+    this.turn = this.winner;
+    this.clear();
+    return`${this.winner} wins!`
+  } else if (this.cols.every(col=>!col.includes(null))) {
+    this.clear()
+    return 'tie game!'
+  } else {
+    this.turn = this.turn === 'X' ?
+      'O' : 'X'
+  }
   console.log(`it is ${this.turn}'s turn`)
+}
+
+Board.prototype.clear = function(){
+  for (let i = 0; i < 7; i++) {
+    this.cols[i] = new Array(6).fill(null)
+  }
+}
+
+Board.prototype.createRow = function(idx) {
+  let row = [];
+  this.cols.forEach(col=>{
+    row.push(col[idx])
+  })
+  return row;
+}
+
+Board.prototype.createMajDiag = function(row, col) {
+  let majDiag = [];
+  while (row > 0 && col > 0) {
+    row--;
+    col--;
+  }
+  while (row < 5 && col < 6) {
+    majDiag.push(this.cols[col][row])
+    row++
+    col++
+  }
+  return majDiag;
+}
+
+Board.prototype.createMinDiag = function(row, col) {
+  let minDiag = [];
+  while (row > 0 && col < 6) {
+    row--;
+    col++;
+  }
+  while (row <= 5 && col >= 0) {
+    minDiag.push(this.cols[col][row])
+    row++;
+    col--;
+  }
+  return minDiag;
+}
+
+Board.prototype.printBoard = function(){
+  console.log('+---------------------+')
+  for (let i = 5; i >=0; i--) {
+    console.log(`| ${this.createRow(i).map(char=>{
+      return char ? char : ' '
+    }).join('  ')} |`)
+  } 
+  console.log('+---------------------+')
+  console.log('  0  1  2  3  4  5  6\n')
 }
 
 Board.prototype.winCheck = function(row, col) {
   let winner = false;
   
   const check = seq => {
-    console.log(seq)
     let counter = 0;
     for(var i = 0; i < seq.length-1; i++) {
-      if (seq[i] && seq[i] !== ' ' && seq[i] === seq[i+1]) {
+      if (seq[i] && seq[i] === seq[i+1]) {
         counter++
-        console.log(counter)
       } else {
         counter = 0
       } 
       if (counter === 3) return winner=seq[i]
     }
   }
+  
+  check(this.cols[col])
   check(this.createRow(row))
-  check(col)
+  check(this.createMajDiag(row, col))
+  check(this.createMinDiag(row, col))
   if (winner) this.winner = winner
 }
-
-Board.prototype.createRow = function(idx) {
-  let row = [];
-  this.cols.forEach(col=>{
-    let piece = col[idx] ? col[idx] : ' '
-    row.push(piece)
-  })
-  return row;
-}
-
-Board.prototype.printBoard = function(){
-  console.log('+---------------------+')
-  for (let i = 5; i >=0; i--) {
-    console.log(`| ${this.createRow(i).join('  ')} |`)
-  } 
-  console.log('+---------------------+')
-  console.log('  0  1  2  3  4  5  6\n')
-}
-
 
 let board = new Board
 let placePiece = board.placePiece.bind(board)
