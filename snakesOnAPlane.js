eefce5c28b65c38cc7a02190c31370c420cfa181
Nathaniel T. Hart <hartbeatnt@gmail.com>
2017-03-13 16:43:42 -0700

    starting snakes on a plane

---
+++ snakesOnAPlane.js
@@ -0,0 +1,39 @@
+/*
+Congratulations!
+
+You saved the day in the easy Kata Snakes on a Plane - #1.
+
+You found all the snakes. The plane landed. Everybody was safe! You're a hero :-)
+
+~~
+
+But you had a return ticket...
+
+And it's the same plane...
+
+And the snakes are back...
+
+Task
+
+Count how many snakes are on the plane
+
+If you are correct  everybody lives!
+If you fail,  we all die!
+No pressure!
+
+Start counting NOW...
+
+1.. 2.. 3.. 4.. hang on didn't you count that one twice? 1.. 2.. Damn! These snakes all look the same!
+
+Notes:
+
+snakes do not touch each other, or themselves
+snakes twist only up, down, left, right (not diagonally)
+snakes all look the same
+snakes can be any length
+*/
+
+function snakesOn(aPlane) {
+  let result = 0;
+  
+}
