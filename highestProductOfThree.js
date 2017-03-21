68dd2fb18c3eecd805396a13e9f73d7d64a20a68
Nathaniel T. Hart <hartbeatnt@gmail.com>
2017-03-20 21:01:45 -0700

    started highest product of three

---
+++ highestProductOfThree.js
@@ -0,0 +1,20 @@
+/*
+Given an array_of_ints, ﬁnd the highest_product 
+you can get from three of the integers.
+The input array_of_ints will always have at 
+least three integers.
+
+Gotchas
+Does your function work with negative numbers? 
+If array_of_ints is [−10,−10,1,3,2] we should 
+return 300 (which we get by taking −10∗−10∗3).
+
+We can do this in O(n) time and O(1) space.
+*/
+
+const highestProductOfThree = arr => {
+
+}
+
+let test = highestProductOfThree([4,2,7,3,7,3,4])
+console.log(test)
