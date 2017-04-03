2109894a6aacf322af68f2a7cc8a8428ff2fff25
Nathaniel T. Hart <hartbeatnt@gmail.com>
2017-04-03 09:28:15 -0700

    added babel to directory. Starting rectangle intersection

---
+++ rectangleIntersection.js
@@ -0,0 +1,29 @@
+/*
+Write a function to ﬁnd the rectangular 
+intersection of two given rectangles. The rectangles 
+will always be "straight" and never "diagonal." More 
+rigorously: each side is parallel with either the 
+x-axis or the y-axis.
+
+They are defined as hash maps like this:
+myRectangle = {
+    'x': 1,
+    'y': 5,
+    'width': 10,
+    'height': 4,
+    }
+Your output rectangle should use this format as well.
+
+Gotchas
+What if there is no intersection?
+
+What if one rectangle is entirely contained in the other? 
+
+What if the rectangles don't really intersect but share 
+an edge? 
+*/
+class Rectangle {
+  constructor(x, y, width, height) {
+    
+  }
+}
