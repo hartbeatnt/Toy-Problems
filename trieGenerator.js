d82cdbd17470df5224ade20616b5c91b304e3ffe
Nathaniel T. Hart <hartbeatnt@gmail.com>
2017-04-05 17:51:37 -0700

    refactored Trie as es6 class and added more functionality

--- trieGenerator.js
+++ trieGenerator.js
@@ -1,47 +1,57 @@
 /*
-write a function that takes an array of strings and stores them to a trie.
+implement a trie.
 */
 
-const strings = [
-  'test',
-  'taste',
-  'taster',
-  'tester',
-  'on',
-  'one',
-  'only'
-]
-
-const saveToTrie = stringArray => {
-  let trie = {};
-  let temp;
-  stringArray.forEach(str=>{
-    temp = trie;
+class Trie {
+  constructor(strings) {
+    let temp;
+    strings.forEach(str=>{
+      this.addLeaf(str)
+    })
+  }
+  addLeaf(str) {
+    let temp = this;
     str.split('').forEach((char, i)=>{
       if (!temp[char]) temp[char] = {};
       if (i === str.length - 1) temp[char].leaf = true
       temp = temp[char]
     })
-  })
-  return trie;
-}
-
-let stringTrie = saveToTrie(strings)
-console.log(JSON.stringify(stringTrie))
-
-const getLeavesFromTrie = trie => {
-  let leaves = [];
-  let recurse = (node,str) => {
-    Object.keys(node).forEach(key=>{
-      if (node[key].leaf) leaves.push(str+key)
-      recurse(node[key], str+key)
-    })
   }
-  recurse(trie, '')
-  return leaves
+  hasLeaf(str) {
+    let temp = this;
+    return str
+      .split('')
+      .every(char=>temp = temp[char]) && !!temp.leaf 
+  }
+  getLeavesFrom(node,str,results) {
+    Object.keys(node).forEach(key=>{
+      if (node[key].leaf) results.push(str+key)
+      this.getLeavesFrom(node[key], str+key, results)
+    })
+    return results
+  }
+  getLeaves(){
+    return this.getLeavesFrom(this,'',[])
+  }
+  getLeavesWithPrefix(str) {
+    let temp = this;
+    return str.split('').every(char=> temp = temp[char])
+      ? this.getLeavesFrom(temp, str, [])
+      : []
+  }
 }
 
-console.log(getLeavesFromTrie(stringTrie))
+
+
+const strings = ['test','taste','taster','tester','on','one','only']
+let stringTrie = new Trie(strings)
+console.log(JSON.stringify(stringTrie))
+console.log(stringTrie.getLeaves())
+stringTrie.addLeaf('YO MAMA')
+console.log(stringTrie.getLeaves())
+console.log(stringTrie.hasLeaf('test'))
+console.log(stringTrie.hasLeaf('tes'))
+console.log(stringTrie.getLeavesWithPrefix('tea'))
 
 /*{
   "t":{
